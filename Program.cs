0c49b5e3b831c352d90ddc28feda676396a80252
Melissa Turbano <melissaturbano@gmail.com>
2022-03-29 17:49:35 -0300

    Adicionando a situação do usuário

--- Program.cs
+++ Program.cs
@@ -20,6 +20,38 @@ namespace calculadoraIMC
             Console.WriteLine("{0}, sua altura é {1} e seu peso é {2}", nome, altura, peso);
 
             Console.WriteLine("Seu IMC é {0}", imc);
+
+            if (imc < 17 ) {
+                Console.WriteLine("Muito abaixo do peso");
+            }
+
+            if (imc >= 17 && imc < 18.50) {
+                Console.WriteLine("Abaixo do peso");
+            }
+
+            if (imc >= 18.50 && imc < 25) {
+                Console.WriteLine("Peso normal");
+            }
+
+            if (imc >= 25 && imc < 30) {
+                Console.WriteLine("Acima do peso");
+            }
+
+            if (imc >= 30 && imc < 35) {
+                Console.WriteLine("Obesidade I");
+            }
+
+            if (imc >= 35 && imc < 40) {
+                Console.WriteLine("Obesidade II (severa)");
+            }
+
+            if (imc > 40) {
+                Console.WriteLine("Obesidade III (mórbida)");
+            }
+
+
+
+
         }
     }
 }
